09e334786cbf5b4490c40262213a3cce4e4925e1
arjunthegoat3 <aginotra3@gmail.com>
2026-03-20 13:05:43 -0700

    Update performanceTask.py

--- performanceTask.py
+++ performanceTask.py
@@ -1,9 +1,11 @@
-5
+
 import pygame
 pygame.init()
 
 question = []
 definition = []
+next = False
+clock = pygame.time.Clock()
 
 numberOfCards = input("How many cards do you want to input? (please input as a number)")
 
@@ -22,3 +24,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            #if the space key is pressed, it will trigger a boolean and when the next question/slide
+            #is shown the boolean will be set back to false.
+            if event.key == pygame.K_SPACE:
+                next = True
+    
+    w.fill((255, 255, 255))
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
