1924df368a463f83f493afc3e87db61e139de07f
arjunthegoat3 <aginotra3@gmail.com>
2026-03-20 12:03:53 -0700

    Update performanceTask.py

--- performanceTask.py
+++ performanceTask.py
@@ -8,11 +8,17 @@ definition = []
 numberOfCards = input("How many cards do you want to input? (please input as a number)")
 
 for i in range(0, int(numberOfCards)):
-    print("\nNEW CARD")
+    print("\n1NEW CARD")
     questionInput = input("What is the question for this card")
     definitionInput = input("what is the answer for the question")
 
     question.append(questionInput)
     definition.append(definitionInput)
 
+w = pygame.display.set_mode((700, 700))
+running = True
+while running:
     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
